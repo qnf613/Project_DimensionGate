f5cc6f17ff05674c715c54c7a3ccd5f66e1cd794
Ricarto <gurwls613@gmail.com>
2022-02-04 18:49:00 -0600

    developing steps for clear condition
    try to connect condition with boss and portal (in progress)

--- Assets/Scripts/System/ClearCondition.cs
+++ Assets/Scripts/System/ClearCondition.cs
@@ -3,54 +3,115 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum StageCleared {yes, yet, over}
+public enum stageCleared {yes, yet, over}
+public enum bossSummoned {yes, no}
+public enum portalStatus {active, inactive, activalbe}
 public class ClearCondition : MonoBehaviour
 {
     public Text[] times;
     public float timeRemain = 600.0f;
     [SerializeField] private GameObject[] bossMonsters;
     [SerializeField] private GameObject bossOfStage;
-    [SerializeField] private bool bossSummoned = false;
-    [SerializeField] protected StageCleared sc;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject portal;
+    [SerializeField] protected bossSummoned bs;
+    [SerializeField] protected stageCleared sc;
+    [SerializeField] protected portalStatus ps;
+    // private int repeatStopper;
+
     // Start is called before the first frame update
     private void Start()
     {
-        sc = StageCleared.yet;
+        player = GameObject.FindGameObjectWithTag("Player");
+        sc = stageCleared.yet;
+        bs = bossSummoned.no;
+        ps = portalStatus.inactive;
         bossOfStage = bossMonsters[Random.Range(0, bossMonsters.Length)];
     }
-
     // Update is called once per frame
     private void Update()
     {
         switch (sc) 
         {
-            case StageCleared.yes:
-                //TODO: spawn portal
+            case stageCleared.yes:
+                //TODO: activate the portal & check bonus rewards depending on time remains
                 break;
-            case StageCleared.yet:
-                if (timeRemain >= 0)
-                {
-                    timeRemain -= Time.deltaTime;
-                    times[0].text = ((int)timeRemain / 60 % 60).ToString();
-                    times[1].text = ((int)timeRemain % 60).ToString();
-                }
+
+            case stageCleared.yet:
+                //countdown for survive limits
+                Countdown();
+                //check player is alive or not
+                CheckPlayer();
                 break;
-            case StageCleared.over:
+            
+            case stageCleared.over:
                 //TODO: call game over animation + UI
                 break;
         }
 
-
-        if (Input.GetKeyDown(KeyCode.E))
+        switch (bs)
         {
-            bossSummoned = true;
-            Debug.Log("boss has been summoned");
+            case bossSummoned.yes:
+                //check boss is alive or not
+                CheckBoss();
+            break;
+
+            case bossSummoned.no:
+                //make portal 'inactive' state while boss never summoned yet
+                DeactivatePortal();
+            break;
+        }
+        
+        switch (ps)
+        {
+            case portalStatus.active:
+                CheckBoss();
+            break;
+
+            case portalStatus.activalbe:
+            break;
+
+            case portalStatus.inactive:
+                CheckBoss();
+            break;
         }
 
-        if (bossSummoned && !bossOfStage.activeInHierarchy)
+    }
+
+    private void Countdown()
+    {
+        if (timeRemain > 0)
         {
-            sc = StageCleared.yes;
-            Debug.Log("boss dead, stage cleared");
+            timeRemain -= Time.deltaTime;
+            times[0].text = ((int)timeRemain / 60 % 60).ToString();
+            times[1].text = ((int)timeRemain % 60).ToString();
         }
+        else
+        {
+            times[0].text = "0";
+            times[1].text = "00";
+            sc = stageCleared.over;
+        }
+    }
+
+    private void CheckPlayer()
+    {
+        if(!player.activeInHierarchy)
+        {
+            sc = stageCleared.over;
+        }
+    }
+
+    private void CheckBoss()
+    {
+        if(!bossOfStage.activeInHierarchy && sc == stageCleared.yet)
+        {
+            sc = stageCleared.yes;
+        }
+    }
+
+    private void DeactivatePortal()
+    {
+        ps = portalStatus.inactive;
     }
 }
