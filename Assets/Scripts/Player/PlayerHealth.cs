99b97a83e787e95c3182a70513dfaa1f5aa4fe3c
Vantrono <382985@colum.edu>
2022-02-10 11:28:10 -0600

    Added health ui and collision
    Added to Demo as well.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -8,11 +9,13 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private float health = 0f;
 
     [SerializeField] private float maxHealth = 100;
-
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Text healthValue;
 
     void Start()
     {
         health = maxHealth;
+        healthSlider.maxValue = maxHealth;
     }
 
     public void UpdateHealth(float mod)
@@ -23,10 +26,20 @@ public class PlayerHealth : MonoBehaviour
         {
             health = maxHealth;
         }
-        else if(health<=0)
+        else if(health<=0f)
         {
             health = 0f;
+            healthSlider.value = health;
             Destroy(gameObject);
+
         }
+    }
+
+    private void OnGUI()
+    {
+        
+        float t = Time.deltaTime / 1f;
+        healthSlider.value = Mathf.Lerp(healthSlider.value, health, t);
+        
     }
 }
