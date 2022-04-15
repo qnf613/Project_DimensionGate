402d16eb763e4045f31d7d3da51363379f9fde66
qnf613 <gurwls613@gmail.com>
2022-04-14 22:11:14 -0500

    xp related scripte updated

--- Assets/Scripts/System/PortalNavigator.cs
+++ Assets/Scripts/System/PortalNavigator.cs
@@ -10,13 +10,16 @@ public class PortalNavigator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        portal = GameObject.FindGameObjectWithTag("Portal");
-        portalDirection = portal.transform.position;
+        //portal = GameObject.FindGameObjectWithTag("Portal");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (portal == null)
+        {
+            portal = GameObject.FindGameObjectWithTag("Portal");
+        }
         portalDirection = portal.transform.position;
         pointingDirection = portalDirection - transform.position;
         float rotz = Mathf.Atan2(pointingDirection.y, pointingDirection.x) * Mathf.Rad2Deg - 90f;
