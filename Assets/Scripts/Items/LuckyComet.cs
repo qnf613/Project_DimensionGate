8a2d49a49c6568862f574a41aff0fb4152ea31bd
Eric Ribeiro <71297100+ErRibeiro@users.noreply.github.com>
2022-04-15 15:25:21 -0500

    LuckyComet now upgrades and increases number of comets

--- Assets/Scripts/Items/LuckyComet.cs
+++ Assets/Scripts/Items/LuckyComet.cs
@@ -8,6 +8,9 @@ public class LuckyComet : Weapon
     [SerializeField] private string name = "Lucky Comet";
     [SerializeField] private string description = "Lucky this comet decided to orbit around you";
     [SerializeField] private float rotationspeed = 30;
+    public GameObject secondComet;
+    public GameObject thirdComet;
+    public GameObject forthComet;
     
     // Start is called before the first frame update
     void Start()
@@ -18,9 +21,23 @@ public class LuckyComet : Weapon
     }
 
     // Update is called once per frame
-    protected override void Update()
+    protected void Update()
     {
         transform.Rotate(new Vector3(0, 0, rotationspeed) * Time.deltaTime);
+
+        if (this.enhancement >= 3)
+        {
+            secondComet.SetActive(true);
+
+        }
+        if (this.enhancement >= 6)
+        {
+            thirdComet.SetActive(true);
+        }
+        if (this.enhancement >= 9)
+        {
+            forthComet.SetActive(true);
+        }
     }
     protected override void Shoot()
     {
