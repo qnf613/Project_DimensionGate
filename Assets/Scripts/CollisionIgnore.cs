8c519557ab65e459d67218fd5c5486f4e015c808
Eric Ribeiro <71297100+ErRibeiro@users.noreply.github.com>
2022-04-15 15:24:49 -0500

    Collision ignore restored

---
+++ Assets/Scripts/CollisionIgnore.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionIgnore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Player;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Physics2D.IgnoreCollision(Player.GetComponent<CapsuleCollider2D>(), this.GetComponent<BoxCollider2D>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
