806d34ce6dcbc7c011653e7f4a21d7df0f4e5736
qnf613 <gurwls613@gmail.com>
2022-07-13 23:35:05 -0500

    Random rarity rewards set up & testing

--- Assets/Scripts/System/RewardS/RewardSystem.cs
+++ Assets/Scripts/System/RewardS/RewardSystem.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class RewardSystem : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> allItemList;
     [SerializeField] private List<GameObject> commonItemList;
     [SerializeField] private List<GameObject> rareItemList;
     [SerializeField] private List<GameObject> epicItemList;
     [SerializeField] private List<GameObject> legendaryItemList;
+    [SerializeField] private List<GameObject> synergyItemList;
     [SerializeField] private List<GameObject> tempList;
+    [SerializeField] private List<GameObject> tempCommonList;
+    [SerializeField] private List<GameObject> tempRareList;
+    [SerializeField] private List<GameObject> tempEpicList;
+    [SerializeField] private List<GameObject> tempLegendaryList;
     [SerializeField] private List<GameObject> equippedList;
     public List<GameObject> rewardsList;
     public GameObject equippedItems;
@@ -20,13 +24,17 @@ public class RewardSystem : MonoBehaviour
     public bool artifactFull = false;
     public GameObject SwapWeaponUI;
     public GameObject SwapArtifactUI;
+
+    private int choosenItemNum;
+
+
     public void Awake()
     {
-        allItemList = Resources.LoadAll<GameObject>("Prefabs/Items").ToList(); //get all items and add it to list
         commonItemList = Resources.LoadAll<GameObject>("Prefabs/Items/Common").ToList();
         rareItemList = Resources.LoadAll<GameObject>("Prefabs/Items/Rare").ToList();
         epicItemList = Resources.LoadAll<GameObject>("Prefabs/Items.Epic").ToList();
         legendaryItemList = Resources.LoadAll<GameObject>("Prefabs/Items/Legendary").ToList();
+        //synergyItemList = Resources.LoadAll<GameObject>("Prefabs/Items/Synergy").ToList();
     }
     // Start is called before the first frame update
     public void Start()
@@ -38,8 +46,6 @@ public class RewardSystem : MonoBehaviour
     public void Update()
     {
         equippedItems = GameObject.Find("Inventory");
-
-
     }
 
     public void MakeRewardList()
@@ -92,15 +98,43 @@ public class RewardSystem : MonoBehaviour
     public void MakeLevelUpRewardList()
     {        
         CheckEquipments();
+        tempLegendaryList = new List<GameObject>(legendaryItemList);
+        tempEpicList = new List<GameObject>(epicItemList);
+        tempRareList = new List<GameObject>(rareItemList);
+        tempCommonList = new List<GameObject>(commonItemList);
         //Make options from all items
-        tempList = new List<GameObject>(allItemList);
-        int choosenItemNum;
         for (int i = 0; i < 3; i++)
         {
-            choosenItemNum = (int)Random.Range(0, tempList.LongCount());
-            rewardsList.Add(tempList[choosenItemNum]);
-            tempList.RemoveAt(choosenItemNum);
-        }
+            int rarityRoll = (int)Random.Range(0, 100);
+            Debug.Log(rarityRoll);
+            if (rarityRoll >= 0 && rarityRoll < 5)
+            {
+                choosenItemNum = (int)Random.Range(0, legendaryItemList.LongCount());
+                rewardsList.Add(tempLegendaryList[choosenItemNum]);
+                tempLegendaryList.RemoveAt(choosenItemNum);
+            }
+
+            else if (rarityRoll >= 5 && rarityRoll < 15)
+            {
+                choosenItemNum = (int)Random.Range(0, epicItemList.LongCount());
+                rewardsList.Add(tempEpicList[choosenItemNum]);
+                tempEpicList.RemoveAt(choosenItemNum);
+            }
+
+            else if (rarityRoll >= 15 && rarityRoll < 40)
+            {
+                choosenItemNum = (int)Random.Range(0, rareItemList.LongCount());
+                rewardsList.Add(tempRareList[choosenItemNum]);
+                tempRareList.RemoveAt(choosenItemNum);
+            }
+
+            else if (rarityRoll >= 40 && rarityRoll <= 100)
+            {
+                choosenItemNum = (int)Random.Range(0, commonItemList.LongCount());
+                rewardsList.Add(tempCommonList[choosenItemNum]);
+                tempCommonList.RemoveAt(choosenItemNum);
+            }
+}
     }
 
     public void CheckEquipments()
